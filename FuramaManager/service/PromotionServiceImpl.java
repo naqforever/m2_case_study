5df24b537c464b13d03895db59157908423963a4
Nguyen Ngoc Anh Quoc <quocnna@gmail.com>
2023-01-04 09:07:42 +0700

    up

--- FuramaManager/service/PromotionServiceImpl.java
+++ FuramaManager/service/PromotionServiceImpl.java
@@ -5,9 +5,11 @@ import m2_case_study.FuramaManager.model.Customer;
 import m2_case_study.FuramaManager.util.ConstantUtil.PROMOTION;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
 public class PromotionServiceImpl implements PromotionService {
@@ -24,25 +26,45 @@ public class PromotionServiceImpl implements PromotionService {
         Map<Customer, Integer> res = new LinkedHashMap<>();
         LocalDate now = LocalDate.now();
 
-        List<Booking> bookings = bookingService.findAll().stream().filter(e -> e.getStartDate().getYear() == now.getYear()
-                && e.getStartDate().getMonthValue() == now.getMonthValue()).collect(Collectors.toList());
+        int totalVoucher = amountOfTenPercent + amountOfTwentyPercent + amountOfFiftyPercent;
+        Stack<Booking> promotionBookings = new Stack<>();
+        bookingService.findAll().stream().filter(e -> e.getStartDate().getYear() == now.getYear() && e.getStartDate().getMonth() == now.getMonth())
+                .collect(Collectors.toSet()).stream().sorted(Comparator.comparing(Booking::getStartDate)).limit(totalVoucher).forEach(f -> promotionBookings.add(f));
 
-        for (int i = bookings.size() - 1; i > 0; i--) {
-            Customer customer = customerService.get(bookings.get(i).getCustomerCode()).get();
-            if (res.size() < amountOfFiftyPercent) {
-                if (!res.containsKey(customer)) {
-                    res.put(customer, PROMOTION.FIFTY_PERCENT);
-                }
-            } else if (res.size() < amountOfFiftyPercent + amountOfTwentyPercent) {
-                if (!res.containsKey(customer)) {
-                    res.put(customer, PROMOTION.TWENTY_PERCENT);
-                }
-            } else if (res.size() < amountOfTenPercent + amountOfTwentyPercent + amountOfFiftyPercent) {
-                if (!res.containsKey(customer)) {
-                    res.put(customer, PROMOTION.TEN_PERCENT);
-                }
+        while (!promotionBookings.isEmpty()){
+            Customer customer = customerService.get(promotionBookings.pop().getCustomerCode()).get();
+
+            if(res.size() < amountOfTenPercent){
+                res.put(customer, PROMOTION.TEN_PERCENT);
+            }
+            else if(res.size() < amountOfTenPercent + amountOfTwentyPercent){
+                res.put(customer, PROMOTION.TWENTY_PERCENT);
+            }
+            else {
+                res.put(customer, PROMOTION.FIFTY_PERCENT);
             }
         }
+
+        /*the way 2 without stack*/
+//        List<Booking> bookings = bookingService.findAll().stream().filter(e -> e.getStartDate().getYear() == now.getYear()
+//                && e.getStartDate().getMonthValue() == now.getMonthValue()).collect(Collectors.toList());
+//
+//        for (int i = bookings.size() - 1; i > 0; i--) {
+//            Customer customer = customerService.get(bookings.get(i).getCustomerCode()).get();
+//            if (res.size() < amountOfFiftyPercent) {
+//                if (!res.containsKey(customer)) {
+//                    res.put(customer, PROMOTION.FIFTY_PERCENT);
+//                }
+//            } else if (res.size() < amountOfFiftyPercent + amountOfTwentyPercent) {
+//                if (!res.containsKey(customer)) {
+//                    res.put(customer, PROMOTION.TWENTY_PERCENT);
+//                }
+//            } else if (res.size() < amountOfTenPercent + amountOfTwentyPercent + amountOfFiftyPercent) {
+//                if (!res.containsKey(customer)) {
+//                    res.put(customer, PROMOTION.TEN_PERCENT);
+//                }
+//            }
+//        }
 
         return res;
     }
